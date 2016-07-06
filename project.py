f91e124da120886c5ab88e20ff01a12f0c6aea3b
Azamat Serek <azamatserek97@gmail.com>
2016-07-06 14:00:52 +0000

    Added a link to info page

--- project.py
+++ project.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template, request
 from datetime import datetime
+
 app = Flask(__name__) 
 @app.route('/', methods=['GET', 'POST'])
 def HelloWorld():
 	if request.method == "POST":
 		url = request.form["url"]
 		author = request.form["author"]
-		print str(datetime.now())
+		current_time = str(datetime.now())
+
+		print current_time
 		print url
 		print author
-       
-	return render_template('form.html', user="Azamat")
+
+		data = {
+			"url": url,
+			"author": author,
+			"time": current_time
+		}
+
+		return render_template("info.html", data=data)
+
+	return render_template('form.html')
 
 if __name__ == '__main__': # if it was called by python interpreter, no imported to another .py file
     app.debug = True
